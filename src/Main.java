09177d90fe38b2f84aeaeac420254fb82fd3526e
DinosaurTim <128mmaropakis@frhsd.com>
2026-09-15 13:50:12 -0400

    We added a scanner for answers

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        //boolean running = true;
         ReadFile file = new ReadFile();
         ArrayList<String> names = new ArrayList<>();
         ArrayList<Integer> quantities = new ArrayList<>();
@@ -23,15 +24,27 @@ public class Main {
             }
             count++;
         }
-        for (String name : names) {
-            System.out.println(name);
-        }
+
         for (Integer quantity : quantities) {
             System.out.println(quantity);
         }
 
         System.out.println("Vending Machine");
         System.out.println("Choose from one of the options below:");
-
+        for (String name : names) {
+            System.out.println(name);
+        }
+        Scanner scan2 = new Scanner(System.in);
+        String answer = scan2.next();
+        for (int i=0; i<5; i++){
+            if (answer.equals(names.get(i))) {
+                if (quantities.get(i) > 0) {
+                    quantities.set(i, quantities.get(i) - 1);
+                }
+                else {
+                    System.out.println("Not enough stuff.");
+                }
+            }
+        }
     }
 }
